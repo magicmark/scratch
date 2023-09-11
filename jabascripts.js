d9e36ee33e588fa5b8a757c8c824fe5fcc1d70be
Mark Larah <mark@larah.me>
2023-09-11 08:41:04 -0500

    Update jabascripts.js

--- jabascripts.js
+++ jabascripts.js
@@ -1,3 +1,8 @@
+#!npx tsx
+import { fileURLToPath } from 'node:url';
+
+const ROOT = fileURLToPath(new URL('..', import.meta.url));
+
 /**
  * async delay ms
  * usage: await delay(4000)
@@ -9,3 +14,10 @@ function delay(duration) {
         }, duration);
     });
 }
+
+main().catch((e) => {
+    console.error(e);
+    // or if you want colorized output, to keep chained causal stack traces, you need util.inspect
+    console.error(chalk.red(util.inspect(e)));
+	process.exit(1);
+});
